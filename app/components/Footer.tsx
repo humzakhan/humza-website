58c27289d0e75629e26f28434fce658b5f66fbd9
Humza Khan <humzakhan@outlook.com>
2026-06-01 16:19:52 -0400

    Footer: add Pakistan flag next to Canada flag

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="footer">
-      <span>Built with <span className="footer-heart">♥</span> in 🇨🇦</span>
+      <span>Built with <span className="footer-heart">♥</span> in 🇨🇦 🇵🇰</span>
     </footer>
   );
 }
